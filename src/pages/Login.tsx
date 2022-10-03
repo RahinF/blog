5bc68ebc876fa423c059da97a1f4ef4f41eb780f
RahinF <s3297352@student.rmit.edu.au>
2022-10-03 20:28:35 +1100

    changed login styling, added accessability, removed password pattern

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,8 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { Link } from "react-router-dom";
 import ILoginForm from "../types/ILoginForm";
 
-const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-
 const Login = () => {
-  const [error, setError] = useState(true);
+  const [error, setError] = useState<boolean>(true);
   const {
     register,
     handleSubmit,
@@ -23,55 +21,70 @@ const Login = () => {
         <h1 className="my-4 text-center text-3xl font-bold">Login</h1>
 
         {error && (
-          <span className="mb-6 block text-center text-error">
+          <span className="mb-6 block text-center text-red-500">
             Something went wrong...
           </span>
         )}
 
         <form
-          className="m-auto flex max-w-sm flex-col items-center gap-8 px-4"
+          className="m-auto mt-6 flex max-w-sm flex-col items-center gap-8 px-4"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="flex w-full flex-col gap-10">
-            <div className="relative flex flex-col gap-2">
+          <div className="flex w-full flex-col gap-4">
+            <div className="flex flex-col">
+              <label htmlFor="email" className="px-1 py-2">
+                Email{" "}
+                <span aria-hidden className="text-red-500">
+                  *
+                </span>
+              </label>
               <input
                 id="email"
                 type="email"
-                className="input-floating peer"
-                placeholder="Email"
+                className="input input-bordered"
+                aria-required
+                aria-invalid={errors.email ? "true" : "false"}
                 {...register("email", { required: "This field is required." })}
               />
-              <label htmlFor="email" className="label-floating">
-                Email
-              </label>
+
               {errors.email && (
-                <p className="text-sm text-error">{errors.email.message}</p>
+                <p role="alert" className="mt-2 text-sm text-red-500">
+                  {errors.email.message}
+                </p>
               )}
             </div>
 
-            <div className="relative flex flex-col gap-2">
+            <div className="flex flex-col">
+              <label htmlFor="password" className="px-1 py-2">
+                Password{" "}
+                <span aria-hidden className="text-red-500">
+                  *
+                </span>
+              </label>
               <input
                 id="password"
                 type="password"
-                className="input-floating peer"
-                placeholder="Password"
+                className="input input-bordered"
+                aria-required
+                aria-invalid={errors.password ? "true" : "false"}
                 {...register("password", {
                   required: "This field is required.",
-                  pattern: {
-                    value: PASSWORD_REGEX,
-                    message:
-                      "Password must contain 8 characters including at least 1 letter and 1 number.",
-                  },
                 })}
               />
-              <label htmlFor="password" className="label-floating">
-                Password
-              </label>
+
               {errors.password && (
-                <p className="text-sm text-error">{errors.password.message}</p>
+                <p role="alert" className="mt-2 text-sm text-red-500">
+                  {errors.password.message}
+                </p>
               )}
             </div>
           </div>
+
+          <span aria-hidden>
+            Fields marked with <span className="text-red-500">*</span> are
+            required.
+          </span>
+
           <div className="flex w-full flex-col gap-6">
             <button className="btn">login</button>
             <span className="self-center">
