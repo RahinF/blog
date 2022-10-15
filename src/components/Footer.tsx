50bd0fdb6bb18d159c81a343c6f1bab044cfb25c
RahinF <s3297352@student.rmit.edu.au>
2022-10-15 14:16:29 +1100

    improved accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="grid h-32 place-items-center border-t">
-      &copy; Copyright 2022, Rahin Farook
+      <span aria-hidden>&copy; Copyright 2022, Rahin Farook</span>
     </footer>
   );
 };
