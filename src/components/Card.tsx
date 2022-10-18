feb7ff705db27f153cd321aedd869ed9d826c9ee
RahinF <s3297352@student.rmit.edu.au>
2022-10-18 18:03:00 +1100

    refactor

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,47 @@ import IPost from "../types/IPost";
 
 interface Props {
   post: IPost;
+  fullSize?: boolean;
 }
 
-const Card = ({ post }: Props) => {
+const Card = ({ post, fullSize }: Props) => {
+  if (fullSize)
+    return (
+      <article className="group">
+        <img
+          className="max-h-60 w-full object-cover transition group-hover:scale-105 group-hover:shadow-lg"
+          src={post.image}
+          alt={post.title}
+        />
+
+        <div className="mt-4 flex flex-col gap-2">
+          <header>
+            <Card.Category category={post.category} />
+            <Card.Heading title={post.title} />
+          </header>
+
+          <p className="text-sm line-clamp-3">{post.text}</p>
+        </div>
+      </article>
+    );
+
   return (
-    <article className="py-10">
-      <span className="text-sm capitalize text-primary">{post.category}</span>
-      <h1 className="font-bold line-clamp-2">{post.title}</h1>
+    <article className="py-10 first:pt-0 last:pb-0">
+      <Card.Category category={post.category} />
+      <Card.Heading title={post.title} />
     </article>
+  );
+};
+
+Card.Heading = ({ title }: { title: string }) => {
+  return <h1 className="font-bold">{title}</h1>;
+};
+
+Card.Category = ({ category }: { category: string }) => {
+  return (
+    <span className="text-sm font-medium capitalize text-primary">
+      {category}
+    </span>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,32 +2,7 @@ import Card from "../components/Card";
 import posts from "../placeholder/post";
 
 const Home = () => {
-  const Post = (
-    <section>
-      <div className="overflow-hidden object-cover">
-        <img
-          src="https://images.unsplash.com/photo-1657299171054-e679f630a776?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt=""
-        />
-      </div>
 
-      <div>
-        <header className="mt-4">
-          <span className="text-sm">Technology</span>
-          <h2 className="font-bold">
-            10 Great Outdoor Deals on Tents, Jackets, and Snow Gear
-          </h2>
-        </header>
-
-        <p className="text-sm line-clamp-2">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
-          facilis reprehenderit qui esse, distinctio adipisci, laudantium ab
-          repudiandae modi iure quaerat doloribus, hic in assumenda labore
-          reiciendis? Ex, praesentium in.
-        </p>
-      </div>
-    </section>
-  );
 
   return (
     <main className="m-auto min-h-screen max-w-screen-lg px-4 xl:px-0">
@@ -39,7 +14,7 @@ const Home = () => {
             alt={posts[0].title}
           />
           <header>
-            <span>{posts[0].date}</span>
+            <div className="text-primary mt-2 mb-2 font-medium">{posts[0].date}</div>
             <h1 className="text-3xl font-bold">{posts[0].title}</h1>
           </header>
         </article>
@@ -51,13 +26,15 @@ const Home = () => {
         </div>
       </div>
 
-      <article className="my-20">
+      <div className="my-20">
         <h1 className="mb-4 text-xl font-bold">Latest Posts</h1>
 
         <div className="grid grid-cols-1 gap-20 sm:grid-cols-2">
-          {[...Array(10)].map((_, index) => Post)}
+        {posts.map(post => (
+            <Card post={post} key={post.id} fullSize />
+          ))}
         </div>
-      </article>
+      </div>
     </main>
   );
 };
