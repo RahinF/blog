437a9e09f3dbf2876a1dac95552d0f46faaa6a05
RahinF <s3297352@student.rmit.edu.au>
2022-10-25 18:06:27 +1100

    app keeps user logged in, redirects when not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Layout from "./components/Layout";
+import PersistLogin from "./features/auth/PersistLogin";
+import RequireAuth from "./features/auth/RequireAuth";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Post from "./pages/Post";
@@ -14,12 +16,16 @@ import Write from "./pages/Write";
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="write" element={<Write />} />
-        <Route path="post/:id" element={<Post />} />
+      <Route element={<PersistLogin />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route element={<RequireAuth />}>
+            <Route path="write" element={<Write />} />
+          </Route>
+          <Route path="post/:id" element={<Post />} />
+        </Route>
       </Route>
     )
   );
